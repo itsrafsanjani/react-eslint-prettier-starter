e422750fc0ef0aadec59786d5ae302c2427d7817
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-10-30 01:55:08 +0600

    fix: jsx files extension changed, code reformated and basic styling added in index.css

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <>
+            <h1>Hello World!</h1>
+            <h6>Give a star if this helps you!</h6>
+        </>
+    );
+}
+
+export default App;
